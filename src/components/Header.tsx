969978b1dc5d5a9d5d2188f22598262edbb10b74
Syed Shoaib Sherazi <syedshoaibsherazi412002@gmail.com>
2024-09-29 01:41:52 +0500

    again update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 function Header() {
   return (
     <div>
-        <nav className=' w-[1450px] h-20 fixed md:h-16 text-white bg-gray-950 flex justify-between rounded-3xl items-center mx-0 md:mx-7 mt-5'>
+        <nav className='w-auto md:w-[1450px] h-20 fixed md:h-16 text-white bg-gray-950 flex justify-between rounded-3xl items-center mx-0 md:mx-7 mt-5'>
             <div className='ml-5 md:ml-10 text-xl md:text-2xl lg:text-3xl font-bold text-pink-600'>SYED SHOAIB SHERAZI</div>
             <div className='flex flex-row text-xl mr-10 lg:mr-20 font-semibold'>
                 
